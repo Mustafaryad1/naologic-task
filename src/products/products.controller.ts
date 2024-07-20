c472e91e2b362910dcc8356319c2099d2eb67b11
Mustafa Ryad <mustafariad03@gmail.com>
2024-07-21 01:50:03 +0300

    Update cron expression to every day

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Logger } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { Cron } from '@nestjs/schedule';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Controller('products')
 export class ProductsController {
   private readonly logger = new Logger(ProductsController.name);
   constructor(private readonly productsService: ProductsService) {}
 
-  @Cron('45 * * * * *')
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   handleUploadCSVProductsCronJob() {
     this.logger.debug('Start uploading products from csv file');
     this.productsService.handleUploadCSVProductsCronJob();
