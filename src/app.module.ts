43124c8992dddcc2fac7b984677b9b4222063831
Mustafa Ryad <mustafariad03@gmail.com>
2024-07-20 13:51:01 +0300

    Set up mongo and config services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseConfigService } from './common/services/mongo/mongoes.services';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
