d794c35c8c0a91092972199a2d21a4fa125374dd
Mustafa Ryad <mustafariad03@gmail.com>
2024-07-20 21:58:03 +0300

    Create global module that handle global services

---
+++ src/common/modules/global/global.module.ts
@@ -0,0 +1,16 @@
+import { BullModule } from '@nestjs/bullmq';
+import { Global, Module } from '@nestjs/common';
+import { BULL_QUEUE_QUEUES } from 'src/common/constants';
+import { CsvImportService } from 'src/common/services/csv/csv-parser.services';
+
+@Global()
+@Module({
+  imports: [
+    BullModule.registerQueue({
+      name: BULL_QUEUE_QUEUES.PRODUCTS.NAME,
+    }),
+  ],
+  providers: [CsvImportService],
+  exports: [CsvImportService],
+})
+export class GlobalModule {}
---
+++ src/common/services/csv/csv-parser.services.ts
@@ -0,0 +1,48 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as csv from 'csv-parser';
+import { CsvProductInterface } from 'src/common/interfaces/csv-interfaces/csv-product.interface';
+import { InjectQueue } from '@nestjs/bullmq';
+import { Queue } from 'bullmq';
+import { BULL_QUEUE_QUEUES } from 'src/common/constants';
+
+@Injectable()
+export class CsvImportService implements OnModuleInit {
+  constructor(
+    @InjectQueue(BULL_QUEUE_QUEUES.PRODUCTS.NAME)
+    private productsQueue: Queue,
+  ) {}
+
+  async onModuleInit() {
+    await this.importProductsCsv();
+  }
+
+  async importProductsCsv(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const filePath = path.join(
+        __dirname,
+        '../../../../src/products/data/images40.csv',
+      );
+      let counter = 0;
+      fs.createReadStream(filePath)
+        .pipe(csv({ separator: '\t' }))
+        .on('data', (data: CsvProductInterface) => {
+          this.productsQueue.add(
+            BULL_QUEUE_QUEUES.PRODUCTS.JOBS.CREATE_PRODUCT,
+            data,
+          );
+        })
+        .on('end', async () => {
+          try {
+            resolve();
+          } catch (error) {
+            reject(error);
+          }
+        })
+        .on('error', (error) => {
+          reject(error);
+        });
+    });
+  }
+}
