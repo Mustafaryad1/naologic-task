9f71d4fd596af8436161f6e603d32a0393a9b094
Mustafa Ryad <mustafariad03@gmail.com>
2024-07-20 21:57:25 +0300

    Added required modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongooseConfigService } from './common/services/mongo/mongoes.services';
 import { ProductsModule } from './products/products.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { GlobalModule } from './common/modules/global/global.module';
+import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
@@ -16,6 +18,17 @@ import { ScheduleModule } from '@nestjs/schedule';
     MongooseModule.forRootAsync({
       useClass: MongooseConfigService,
     }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        connection: {
+          host: configService.get('REDIS_HOST'),
+          port: configService.get('REDIS_PORT'),
+        },
+      }),
+      inject: [ConfigService],
+    }),
+    GlobalModule,
     ProductsModule,
   ],
   controllers: [AppController],
